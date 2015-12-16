f7ce13dd8f1d9b39877dac06aa9f7423fdcf44e3
Lee Crosby <lee.marreros@pucp.pe>
2015-12-16 13:14:49 -0800

    [Feature] Adds upload work page

--- client/components/Work.js
+++ client/components/Work.js
@@ -12,8 +12,20 @@ class Work extends React.Component{
 
   render() {
     return (
-      <div className="wrapperLogin">
-        Work
+      <div className="wrapperArticles">
+        <div><div className="wrappH1"><h1>All Products</h1></div></div>
+        <div><input className="inputBox" type="text" placeholder="Product's title"></input></div>
+        <div className="twoInputBoxes"><input className="inputBox" type="number" placeholder="Price ($124.00)"></input><input className="inputBox" type="text" placeholder="Category"></input></div>
+        <div><textarea className="textArea" rows="4" placeholder="Describe what are you thoughts here!" cols="50"></textarea></div>
+        <div><input className="inputBox" type="text" placeholder="Tags (example: metal, glass, gold)"></input></div>
+        <div className="addFiles">
+          <div className="wrapperButtonsArticle"><span className="addFilesButton"><input className="inputFile" type="file" name="photo"/><h1>+ Add Files</h1></span>
+          <img className="cameraButton" src={'../img/camera-publish-icon.png'}/></div>
+        </div>
+        <div className="wrapperEndButtons">
+          <div><span><h1>SAVE CHANGES</h1></span></div>
+          <div><span><h1>PUBLISH</h1></span></div>
+        </div>
       </div>
       );
   }
